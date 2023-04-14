893af74033719536750b163011c02a3b641725bd
Yuttakhan B <yuttakhanbaingen@gmail.com>
2023-04-14 20:37:49 +0700

    :sparkles: Add simple summarize text flow as poc

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin } from "obsidian";
+import { HfInference } from "@huggingface/inference";
+import { Editor, MarkdownView, Notice, Plugin } from "obsidian";
 import { HuggingMDSettingTab } from "src/HuggingMDSettingTab";
 
 const DEFAULT_SETTINGS: HuggingMDSettings = {
@@ -7,74 +8,38 @@ const DEFAULT_SETTINGS: HuggingMDSettings = {
 
 export default class HuggingMD extends Plugin {
 	settings: HuggingMDSettings;
+	hf: HfInference;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon(
-			"dice",
-			"Sample Plugin",
-			(evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				new Notice("This is a notice!");
-			}
-		);
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass("my-plugin-ribbon-class");
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Status Bar Text");
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			id: "summarization-command",
+			name: "Add Summarization command",
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				const selectedText = editor.getSelection();
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				new Notice(`Sending inputs to HuggingFace for summarize.`);
+
+				const response = await this.hf.summarization({
+					model: "facebook/bart-large-cnn",
+					inputs: selectedText,
+				});
+				editor.replaceRange(
+					`\n\n🤖 : ***${response.summary_text}***\n\n`,
+					editor.getCursor("to")
+				);
+
+				new Notice(`Summarized requested content.`);
 			},
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new HuggingMDSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
 			console.log("click", evt);
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(
 			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
 		);
@@ -88,25 +53,11 @@ export default class HuggingMD extends Plugin {
 			DEFAULT_SETTINGS,
 			await this.loadData()
 		);
+
+		this.hf = new HfInference(this.settings.apiKey);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText("Woah!");
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
 	}
 }
